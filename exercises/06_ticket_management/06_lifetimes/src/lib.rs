1c704f28568bbfd0194314acd9a96a28ea334540
AbdulbasitSaid <mrspeeed518@gmail.com>
2026-06-06 21:19:20 +0100

    feat: 06_lifetimes

--- exercises/06_ticket_management/06_lifetimes/src/lib.rs
+++ exercises/06_ticket_management/06_lifetimes/src/lib.rs
@@ -1,3 +1,5 @@
+use std::slice::Iter;
+
 use ticket_fields::{TicketDescription, TicketTitle};
 
 // TODO: Implement the `IntoIterator` trait for `&TicketStore` so that the test compiles and passes.
@@ -31,11 +33,19 @@ impl TicketStore {
         self.tickets.push(ticket);
     }
 
-    pub fn iter(&self) -> std::slice::Iter<Ticket> {
+    pub fn iter(&self) -> std::slice::Iter<'_, Ticket> {
         self.tickets.iter()
     }
 }
 
+impl<'a> IntoIterator for &'a TicketStore {
+    type Item = &'a Ticket;
+
+    type IntoIter = Iter<'a, Ticket>;
+    fn into_iter(self) -> Self::IntoIter {
+        self.tickets.iter()
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
